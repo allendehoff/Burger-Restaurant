a19cec987972a2c1b6850261ef26031a9420a427
Allen DeHoff <allen.dehoff21@gmail.com>
2020-10-05 13:54:22 -0600

    all dependencies required where needed and exports declared.

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+//Object holding all SQL functions
+const orm = {
+
+}
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+// Object holding code to use the ORM functions from orm.js
+const burger = {
+
+}
+
+// Export database functions for use in the controller
+module.exports = burger
